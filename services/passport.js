91411399ec57b4ea9cd5198317e81211a0b856e0
Max Nikolenko <maxim.nikolenko@gmail.com>
2018-06-17 23:12:02 +0300

    Makes the Passport Strategy trust a Heroku proxy

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,8 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy: true
         },
         async (accessToken, refreshToken, profile, done) => {
             const user = await User.find_or_create_from_google(profile);
