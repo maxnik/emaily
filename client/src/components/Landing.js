07288f0669f5b8f1efce0cb25d991fed6cd59623
Max Nikolenko <maxim.nikolenko@gmail.com>
2018-06-24 12:40:55 +0300

    Adds styles to a card

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Icon from 'react-feather';
 
 class Landing extends React.Component {
     render() {
@@ -7,15 +8,35 @@ class Landing extends React.Component {
                 <div class="container">
                     <div class="row">
                         <div class="col">
-                            <div class="box">Some Callout Text</div>
-                        </div>
+                            <div class="card">
+                                <div class="card-aside">
+                                    <button class="card-skill">Italian</button>
 
-                        <div class="col">
-                            <div class="box">Some Callout Text</div>
-                        </div>
+                                    <span class="card-difficulty">
+                                        Beginner Difficulty
+                                    </span>
+                                </div>
 
-                        <div class="col">
-                            <div class="box">Some Callout Text</div>
+                                <div class="card-primary">
+                                    <a href="#">
+                                        <h3 class="card-title">
+                                            Prima Lezione (Assimil Italian)
+                                        </h3>
+                                    </a>
+
+                                    <div class="card-meta">
+                                        <div>
+                                            <Icon.Archive />
+                                            5 cards
+                                        </div>
+
+                                        <div>
+                                            <Icon.Clock />
+                                            10 days ago
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
                         <div class="col">
@@ -24,6 +45,14 @@ class Landing extends React.Component {
                     </div>
 
                     <div class="row">
+                        <div class="col">
+                            <div class="box">Some Callout Text</div>
+                        </div>
+
+                        <div class="col">
+                            <div class="box">Some Callout Text</div>
+                        </div>
+
                         <div class="col">
                             <div class="box">Some Callout Text</div>
                         </div>
